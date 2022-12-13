2e3756b317c3d462eea7427568d1602968608c2d
InHiBiToRs <Tanakrit64566@gmail.com>
2022-12-13 16:03:02 +0700

    lab7_2_complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,26 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout << "Please input k: ";
+	cin >> k;
+	if(k>0){
+		int l = 0,result = 0;
+		while(l < 5000){
+			if(P[l]%k == 0){
+				result = result - P[l];
+			}else{
+				result = result + P[l];
+			}
+			l++;
+		}
+		cout << "Result = " <<result;
+	}else{
+		cout << "Invalid input!!!";
+	}
 	return 0;
 }
